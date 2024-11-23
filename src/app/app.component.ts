e29c6437e9ec3ff8bf48974aa26ad7d0b8be245c
Argot <argot-vierer.0e@icloud.com>
2024-11-23 01:18:13 +0100

    zaakceptowane

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from './logo/logo.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  
+  
 })
 export class AppComponent {
   title = 'rezerwacje';
